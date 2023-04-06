e0761e462d8470fcac8e1105dc0dd85458015e19
binghe001 <1028386804@qq.com>
2023-04-06 18:39:22 +0800

    提交《Spring核心技术》第47章文章

---
+++ spring-annotation-chapter-47/src/main/java/io/binghe/spring/annotation/chapter47/controller/PathVariableController.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.spring.annotation.chapter47.controller;
+
+import io.binghe.spring.annotation.chapter47.bean.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description @PathVariable注解案例启动类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+@RestController
+public class PathVariableController {
+
+    @RequestMapping(value = "/user/{id}/{name}")
+    public String getUser(@PathVariable("id") String id, @PathVariable("name") String name){
+        return new User(id, name).toString();
+    }
+}
