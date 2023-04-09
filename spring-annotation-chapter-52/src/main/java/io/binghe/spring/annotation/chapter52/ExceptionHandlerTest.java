eb9e92c79ee8b0903a636bb8ec67dd8dc53328ec
binghe001 <1028386804@qq.com>
2023-04-09 23:19:29 +0800

    提交《Spring核心技术》第52章代码

---
+++ spring-annotation-chapter-52/src/main/java/io/binghe/spring/annotation/chapter52/ExceptionHandlerTest.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.spring.annotation.chapter52;
+
+import io.binghe.spring.annotation.springmvc.starter.TomcatStarter;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description @ExceptionHandler注解案例启动类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+public class ExceptionHandlerTest {
+    public static void main(String[] args) {
+        TomcatStarter.start(8888);
+    }
+}
