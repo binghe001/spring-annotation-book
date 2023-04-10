cb5eb091a41542d7d889142c2137f081919b9011
binghe001 <1028386804@qq.com>
2023-04-10 09:52:31 +0800

    提交《Spring核心技术》第53章文章

---
+++ spring-annotation-chapter-53/src/main/java/io/binghe/spring/annotation/chapter53/controller/InitBinderController.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2022-9999 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.binghe.spring.annotation.chapter53.controller;
+
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.beans.PropertyEditorSupport;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * @author binghe(微信 : hacker_binghe)
+ * @version 1.0.0
+ * @description @InitBinder注解Controller类
+ * @github https://github.com/binghe001
+ * @copyright 公众号: 冰河技术
+ */
+@RestController
+public class InitBinderController {
+
+    @RequestMapping(value = "/hello")
+    public String hello(String name){
+        return "hello " + name;
+    }
+
+    @InitBinder
+    public void bindData(WebDataBinder binder){
+        binder.registerCustomEditor(String.class, new PropertyEditorSupport(){
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(text + " "+ new DateFormatter("yyyy-MM-dd HH:mm:ss").print(new Date(), Locale.CHINA));
+            }
+        });
+    }
+}
